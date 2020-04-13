f095b964f2a31acdf6e9cabbf8f503569cbcd8f0
ryansmane <ryanzimmer8@gmail.com>
2020-04-12 20:09:20 -0400

    Updates

---
+++ precache-manifest.54d1c36aa55ddd445f015daf16c96f36.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4375535250228be8cc1b217ff9c5ccd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "936a400acb60aadf8c04",
+    "url": "/static/css/main.fbd7df7e.chunk.css"
+  },
+  {
+    "revision": "d9061cf0918cd34f320a",
+    "url": "/static/js/2.03ec32db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.03ec32db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "936a400acb60aadf8c04",
+    "url": "/static/js/main.30507dc1.chunk.js"
+  },
+  {
+    "revision": "91f402dc4496368e94e5",
+    "url": "/static/js/runtime-main.33110160.js"
+  },
+  {
+    "revision": "758ff880762fcfe1657f35761c294bea",
+    "url": "/static/media/bookmaker.758ff880.JPG"
+  },
+  {
+    "revision": "764f60d2771b1fe0416cba5586e6cd44",
+    "url": "/static/media/cinopsis.764f60d2.png"
+  },
+  {
+    "revision": "28e023f4dc37bac78cba887c21ee019a",
+    "url": "/static/media/githublogo.28e023f4.png"
+  },
+  {
+    "revision": "2f7f97b780a7d43631aff94c56b53941",
+    "url": "/static/media/gol.2f7f97b7.png"
+  },
+  {
+    "revision": "4ef759b68a970a222df6bf1e8ab57ed5",
+    "url": "/static/media/hangman.4ef759b6.png"
+  },
+  {
+    "revision": "a98efd3b650b746d071d2172ef20e631",
+    "url": "/static/media/picasso_samp.a98efd3b.png"
+  },
+  {
+    "revision": "fa08b49b8ee8a073e2ce00b3ca988a7b",
+    "url": "/static/media/stubborntrader.fa08b49b.png"
+  },
+  {
+    "revision": "b93088ead729b839e988ab64d4c9b873",
+    "url": "/static/media/tranquil.b93088ea.png"
+  }
+]);
